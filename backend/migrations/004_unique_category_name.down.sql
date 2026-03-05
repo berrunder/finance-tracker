c0ed9a0b9831383400971421a8ad5ef16e9812b3
Alexander Cherkashin <alexander.cherkashin@csssr.com>
2026-03-05 20:47:49 +0300

    fix(backend): Correct unique index for categories

--- backend/migrations/004_unique_category_name.down.sql
+++ backend/migrations/004_unique_category_name.down.sql
@@ -1,2 +1,3 @@
-ALTER TABLE categories DROP CONSTRAINT categories_user_id_name_type_key;
+DROP INDEX IF EXISTS categories_user_id_name_type_top_level;
+DROP INDEX IF EXISTS categories_user_id_parent_id_name_type;
 ALTER TABLE categories ADD CONSTRAINT categories_user_id_parent_id_name_type_key UNIQUE (user_id, parent_id, name, type);
--- backend/migrations/004_unique_category_name.up.sql
+++ backend/migrations/004_unique_category_name.up.sql
@@ -1,2 +1,3 @@
 ALTER TABLE categories DROP CONSTRAINT categories_user_id_parent_id_name_type_key;
-ALTER TABLE categories ADD CONSTRAINT categories_user_id_name_type_key UNIQUE (user_id, name, type);
+CREATE UNIQUE INDEX categories_user_id_name_type_top_level ON categories (user_id, name, type) WHERE parent_id IS NULL;
+CREATE UNIQUE INDEX categories_user_id_parent_id_name_type ON categories (user_id, parent_id, name, type) WHERE parent_id IS NOT NULL;
